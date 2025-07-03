0f9838195c1bb164ea39db6b8edef9b2536a39b2
hawsawi222 <hawsawi0077x@gmail.com>
2025-07-03 23:38:34 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,7 +1,7 @@
 export default [
   {
-    channelId: "1386157633786085386",
-    serverId: "1346788139724963840",
+    channelId: "1388953501534781471", 
+    serverId: "1133667699151618130",
     token: process.env.token1,
     selfDeaf: false,
     autoReconnect: {
