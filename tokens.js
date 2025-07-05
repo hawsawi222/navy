5745bb2f6fa6312ffb72aad6468a5a15340e41ec
hawsawi222 <hawsawi0077x@gmail.com>
2025-07-05 15:54:32 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -14,4 +14,36 @@ export default [
     },
     selfMute: false,
   },
+  export default [
+  {
+    channelId: "1388953501534781471",
+    serverId: "1133667699151618130",
+    token: process.env.token2,
+    selfDeaf: false,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  },
+  export default [
+  {
+    channelId: "1388953501534781471",
+    serverId: "1133667699151618130",
+    token: process.env.token3,
+    selfDeaf: false,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  },
 ];
