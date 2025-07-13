ab9a4380146cbc6186338a17a511642a57713e06
hawsawi222 <hawsawi0077x@gmail.com>
2025-07-13 19:54:47 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
-    channelId: "1243695238392840273",
-    serverId: "1109636583642103903",
+    channelId: "1393396660763099156",
+    serverId: "1243695238392840273",
     token: process.env.token1,
     selfDeaf: false,
     autoReconnect: {
@@ -15,8 +15,8 @@ module.exports = [
     selfMute: false,
   },
   {
-    channelId: "1243695238392840273",
-    serverId: "1109636583642103903",
+    channelId: "1393396660763099156",
+    serverId: "1243695238392840273",
     token: process.env.token2,
     selfDeaf: false,
     autoReconnect: {
@@ -30,8 +30,8 @@ module.exports = [
     selfMute: false,
   },
   {
-    channelId: "1243695238392840273",
-    serverId: "1109636583642103903",
+    channelId: "1393396660763099156",
+    serverId: "1243695238392840273",
     token: process.env.token3,
     selfDeaf: false,
     autoReconnect: {
