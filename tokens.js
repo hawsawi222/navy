2cd934ade03141958b8c979fe609fcab4c87306e
hawsawi222 <hawsawi0077x@gmail.com>
2025-10-02 22:27:40 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -44,10 +44,40 @@ module.exports = [
     },
     selfMute: false,
   },
-   {
+  {
     channelId: "1395923052545577040",
     serverId: "1394436161182629998",
     token: process.env.token4,
+    selfDeaf: false,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  },
+  {
+    channelId: "1423372345866981469", // روم جديد
+    serverId: "1423372344650502156", // سيرفر جديد
+    token: process.env.token5,
+    selfDeaf: false,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  },
+  {
+    channelId: "1423372345866981469", // روم جديد
+    serverId: "1423372344650502156", // سيرفر جديد
+    token: process.env.token6,
     selfDeaf: false,
     autoReconnect: {
       enabled: true,
