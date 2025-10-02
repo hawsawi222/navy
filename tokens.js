af2c56503918639198c8762c4fd9e3c9785b5635
hawsawi222 <hawsawi0077x@gmail.com>
2025-10-02 22:35:13 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,92 +1,92 @@
 module.exports = [
   {
-    channelId: "1413287404252696768",
-    serverId: "248216205943373835",
+    channelId: "1262047149282168862",
+    serverId: "1237069373764665437",
     token: process.env.token1,
     selfDeaf: false,
     autoReconnect: {
       enabled: true,
       delay: 30000,
-      maxRetries: 5,
+      maxRetries: 5
     },
     presence: {
-      status: "invisible",
+      status: "invisible"
     },
-    selfMute: false,
+    selfMute: false
   },
   {
-    channelId: "1413287404252696768",
-    serverId: "248216205943373835",
+    channelId: "1262047149282168862",
+    serverId: "1237069373764665437",
     token: process.env.token2,
     selfDeaf: false,
     autoReconnect: {
       enabled: true,
       delay: 30000,
-      maxRetries: 5,
+      maxRetries: 5
     },
     presence: {
-      status: "invisible",
+      status: "invisible"
     },
-    selfMute: false,
+    selfMute: false
   },
   {
-    channelId: "1413287404252696768",
-    serverId: "248216205943373835",
+    channelId: "1262047149282168862",
+    serverId: "1237069373764665437",
     token: process.env.token3,
     selfDeaf: false,
     autoReconnect: {
       enabled: true,
       delay: 30000,
-      maxRetries: 5,
+      maxRetries: 5
     },
     presence: {
-      status: "invisible",
+      status: "invisible"
     },
-    selfMute: false,
+    selfMute: false
   },
   {
-    channelId: "1395923052545577040",
-    serverId: "1394436161182629998",
+    channelId: "1262047149282168862",
+    serverId: "1237069373764665437",
     token: process.env.token4,
     selfDeaf: false,
     autoReconnect: {
       enabled: true,
       delay: 30000,
-      maxRetries: 5,
+      maxRetries: 5
     },
     presence: {
-      status: "invisible",
+      status: "invisible"
     },
-    selfMute: false,
+    selfMute: false
   },
   {
-    channelId: "1423372345866981469", // روم جديد
-    serverId: "1423372344650502156", // سيرفر جديد
+    channelId: "1262047149282168862",
+    serverId: "1237069373764665437",
     token: process.env.token5,
     selfDeaf: false,
     autoReconnect: {
       enabled: true,
       delay: 30000,
-      maxRetries: 5,
+      maxRetries: 5
     },
     presence: {
-      status: "invisible",
+      status: "invisible"
     },
-    selfMute: false,
+    selfMute: false
   },
   {
-    channelId: "1423372345866981469", // روم جديد
-    serverId: "1423372344650502156", // سيرفر جديد
+    channelId: "1262047149282168862",
+    serverId: "1237069373764665437",
     token: process.env.token6,
     selfDeaf: false,
     autoReconnect: {
       enabled: true,
       delay: 30000,
-      maxRetries: 5,
+      maxRetries: 5
     },
     presence: {
-      status: "invisible",
+      status: "invisible"
     },
-    selfMute: false,
-  },
+    selfMute: false
+  }
 ];
